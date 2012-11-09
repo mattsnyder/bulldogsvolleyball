4f8edd1fbaffd5ae934831e261a8676bc42aa9fa
Matt Snyder <snyder2112@me.com>
2012-11-08 22:30:21 -0500

    Init with Locomotive CMS

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'locomotive_cms', '~> 2.0.0.rc12', :require => 'locomotive/engine'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',     '~> 3.2.4'
+  gem 'coffee-rails',   '~> 3.2.2'
+  gem 'compass-rails',  '~> 1.0.2'
+  gem 'uglifier',       '~> 1.2.4'
+end
+
+gem 'unicorn', :group => 'development'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bulldogsvolleyball::Application.config.secret_token = 'afcc9193ca61b8384ed47ee10ea185c93192a823cabf343e00226ab99c155738fea73a144f01792873d167b7e6c3f38a25176b4dbde76e8437ae38f3db8fe02c'
